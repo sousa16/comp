3e0a9a177fd9d9a2f1bf213cde0fb113268f1f7a
sousa <joao.p.c.sousa@tecnico.ulisboa.pt>
2024-05-13 10:31:28 +0100

    fix: fixed program_node

--- ast/program_node.h
+++ ast/program_node.h
@@ -5,23 +5,21 @@
 
 namespace til {
 
-  /**
-   * Class for describing program nodes.
-   */
-  class program_node : public cdk::basic_node {
+/**
+ * Class for describing program nodes.
+ */
+class program_node : public cdk::basic_node {
     cdk::basic_node *_statements;
 
-  public:
-    program_node(int lineno, cdk::basic_node *statements) :
-        cdk::basic_node(lineno), _statements(statements) {
+   public:
+    program_node(int lineno, cdk::basic_node *statements) : cdk::basic_node(lineno), _statements(statements) {
     }
 
     cdk::basic_node *statements() { return _statements; }
 
     void accept(basic_ast_visitor *sp, int level) { sp->do_program_node(this, level); }
+};
 
-  };
-
-} // til
+}  // namespace til
 
 #endif
