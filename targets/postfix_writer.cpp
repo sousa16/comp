3d9a4e195287676cabcb4e46ba47567487073032
sousa <joao.p.c.sousa@tecnico.ulisboa.pt>
2024-05-30 01:26:59 +0100

    fix: fixed binaries pw

--- targets/postfix_writer.cpp
+++ targets/postfix_writer.cpp
@@ -187,17 +187,38 @@ void til::postfix_writer::do_sub_node(cdk::sub_node* const node, int lvl) {
         _pf.DIV();
     }
 }
-void til::postfix_writer::do_mul_node(cdk::mul_node* const node, int lvl) {
+
+void til::postfix_writer::prepareIDBinaryExpression(cdk::binary_operation_node* const node, int lvl) {
     ASSERT_SAFE_EXPRESSIONS;
+
     node->left()->accept(this, lvl);
+    if (node->left()->is_typed(cdk::TYPE_INT) && node->right()->is_typed(cdk::TYPE_DOUBLE)) {
+        _pf.I2D();
+    }
+
     node->right()->accept(this, lvl);
-    _pf.MUL();
+    if (node->left()->is_typed(cdk::TYPE_DOUBLE) && node->right()->is_typed(cdk::TYPE_INT)) {
+        _pf.I2D();
+    }
+}
+
+void til::postfix_writer::do_mul_node(cdk::mul_node* const node, int lvl) {
+    prepareIDBinaryExpression(node, lvl);
+
+    if (node->is_typed(cdk::TYPE_DOUBLE)) {
+        _pf.DMUL();
+    } else {
+        _pf.MUL();
+    }
 }
 void til::postfix_writer::do_div_node(cdk::div_node* const node, int lvl) {
-    ASSERT_SAFE_EXPRESSIONS;
-    node->left()->accept(this, lvl);
-    node->right()->accept(this, lvl);
-    _pf.DIV();
+    prepareIDBinaryExpression(node, lvl);
+
+    if (node->is_typed(cdk::TYPE_DOUBLE)) {
+        _pf.DDIV();
+    } else {
+        _pf.DIV();
+    }
 }
 void til::postfix_writer::do_mod_node(cdk::mod_node* const node, int lvl) {
     ASSERT_SAFE_EXPRESSIONS;
@@ -205,40 +226,38 @@ void til::postfix_writer::do_mod_node(cdk::mod_node* const node, int lvl) {
     node->right()->accept(this, lvl);
     _pf.MOD();
 }
+
+void til::postfix_writer::prepareIDBinaryComparisonExpression(cdk::binary_operation_node* const node, int lvl) {
+    prepareIDBinaryExpression(node, lvl);
+
+    if (node->left()->is_typed(cdk::TYPE_DOUBLE) || node->right()->is_typed(cdk::TYPE_DOUBLE)) {
+        _pf.DCMP();
+        _pf.INT(0);
+    }
+}
+
 void til::postfix_writer::do_lt_node(cdk::lt_node* const node, int lvl) {
-    ASSERT_SAFE_EXPRESSIONS;
-    node->left()->accept(this, lvl);
-    node->right()->accept(this, lvl);
+    prepareIDBinaryComparisonExpression(node, lvl);
     _pf.LT();
 }
 void til::postfix_writer::do_le_node(cdk::le_node* const node, int lvl) {
-    ASSERT_SAFE_EXPRESSIONS;
-    node->left()->accept(this, lvl);
-    node->right()->accept(this, lvl);
+    prepareIDBinaryComparisonExpression(node, lvl);
     _pf.LE();
 }
 void til::postfix_writer::do_ge_node(cdk::ge_node* const node, int lvl) {
-    ASSERT_SAFE_EXPRESSIONS;
-    node->left()->accept(this, lvl);
-    node->right()->accept(this, lvl);
+    prepareIDBinaryComparisonExpression(node, lvl);
     _pf.GE();
 }
 void til::postfix_writer::do_gt_node(cdk::gt_node* const node, int lvl) {
-    ASSERT_SAFE_EXPRESSIONS;
-    node->left()->accept(this, lvl);
-    node->right()->accept(this, lvl);
+    prepareIDBinaryComparisonExpression(node, lvl);
     _pf.GT();
 }
 void til::postfix_writer::do_ne_node(cdk::ne_node* const node, int lvl) {
-    ASSERT_SAFE_EXPRESSIONS;
-    node->left()->accept(this, lvl);
-    node->right()->accept(this, lvl);
+    prepareIDBinaryComparisonExpression(node, lvl);
     _pf.NE();
 }
 void til::postfix_writer::do_eq_node(cdk::eq_node* const node, int lvl) {
-    ASSERT_SAFE_EXPRESSIONS;
-    node->left()->accept(this, lvl);
-    node->right()->accept(this, lvl);
+    prepareIDBinaryComparisonExpression(node, lvl);
     _pf.EQ();
 }
 
