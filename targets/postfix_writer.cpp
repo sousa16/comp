1bfaf4bbec13aaa14ebca2974eeb33cedd0617fc
sousa <joao.p.c.sousa@tecnico.ulisboa.pt>
2024-05-30 01:24:19 +0100

    fix: fixed add pw

--- targets/postfix_writer.cpp
+++ targets/postfix_writer.cpp
@@ -128,9 +128,30 @@ void til::postfix_writer::do_unary_plus_node(cdk::unary_plus_node* const node, i
 
 void til::postfix_writer::do_add_node(cdk::add_node* const node, int lvl) {
     ASSERT_SAFE_EXPRESSIONS;
+
     node->left()->accept(this, lvl);
+    if (node->is_typed(cdk::TYPE_DOUBLE) && node->left()->is_typed(cdk::TYPE_INT)) {
+        _pf.I2D();
+    } else if (node->is_typed(cdk::TYPE_POINTER) && node->left()->is_typed(cdk::TYPE_INT)) {
+        auto ref = cdk::reference_type::cast(node->type());
+        _pf.INT(std::max(static_cast<size_t>(1), ref->referenced()->size()));
+        _pf.MUL();
+    }
+
     node->right()->accept(this, lvl);
-    _pf.ADD();
+    if (node->is_typed(cdk::TYPE_DOUBLE) && node->right()->is_typed(cdk::TYPE_INT)) {
+        _pf.I2D();
+    } else if (node->is_typed(cdk::TYPE_POINTER) && node->right()->is_typed(cdk::TYPE_INT)) {
+        auto ref = cdk::reference_type::cast(node->type());
+        _pf.INT(std::max(static_cast<size_t>(1), ref->referenced()->size()));
+        _pf.MUL();
+    }
+
+    if (node->is_typed(cdk::TYPE_DOUBLE)) {
+        _pf.DADD();
+    } else {
+        _pf.ADD();
+    }
 }
 void til::postfix_writer::do_sub_node(cdk::sub_node* const node, int lvl) {
     ASSERT_SAFE_EXPRESSIONS;
