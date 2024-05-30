f081b32e1c54f7cfddc38db18094399d1c90c99e
sousa <joao.p.c.sousa@tecnico.ulisboa.pt>
2024-05-30 01:18:33 +0100

    fix: revert changes

--- targets/postfix_writer.cpp
+++ targets/postfix_writer.cpp
@@ -34,11 +34,15 @@ void til::postfix_writer::do_sequence_node(cdk::sequence_node* const node, int l
 //---------------------------------------------------------------------------
 
 void til::postfix_writer::do_integer_node(cdk::integer_node* const node, int lvl) {
-    if (inFunction()) {
+    _pf.INT(node->value());  // push an integer
+
+    /*
+     * if (inFunction()) {
         _pf.INT(node->value());  // push an integer
     } else {
         _pf.SINT(node->value());
     }
+     */
 }
 
 void til::postfix_writer::do_double_node(cdk::double_node* const node, int lvl) {
