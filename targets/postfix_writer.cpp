5376ecc2b9c24c186eb9b0a55eabc651a8a17968
sousa <joao.p.c.sousa@tecnico.ulisboa.pt>
2024-05-30 01:25:29 +0100

    fix: fixed sub pw

--- targets/postfix_writer.cpp
+++ targets/postfix_writer.cpp
@@ -155,9 +155,37 @@ void til::postfix_writer::do_add_node(cdk::add_node* const node, int lvl) {
 }
 void til::postfix_writer::do_sub_node(cdk::sub_node* const node, int lvl) {
     ASSERT_SAFE_EXPRESSIONS;
+
     node->left()->accept(this, lvl);
+    if (node->is_typed(cdk::TYPE_DOUBLE) && node->left()->is_typed(cdk::TYPE_INT)) {
+        _pf.I2D();
+    } else if (node->is_typed(cdk::TYPE_POINTER) && node->left()->is_typed(cdk::TYPE_INT)) {
+        auto ref = cdk::reference_type::cast(node->type());
+        _pf.INT(std::max(static_cast<size_t>(1), ref->referenced()->size()));
+        _pf.MUL();
+    }
+
     node->right()->accept(this, lvl);
-    _pf.SUB();
+    if (node->is_typed(cdk::TYPE_DOUBLE) && node->right()->is_typed(cdk::TYPE_INT)) {
+        _pf.I2D();
+    } else if (node->is_typed(cdk::TYPE_POINTER) && node->right()->is_typed(cdk::TYPE_INT)) {
+        auto ref = cdk::reference_type::cast(node->type());
+        _pf.INT(std::max(static_cast<size_t>(1), ref->referenced()->size()));
+        _pf.MUL();
+    }
+
+    if (node->is_typed(cdk::TYPE_DOUBLE)) {
+        _pf.DSUB();
+    } else {
+        _pf.SUB();
+    }
+
+    if (node->left()->is_typed(cdk::TYPE_POINTER) && node->right()->is_typed(cdk::TYPE_POINTER)) {
+        // the difference between two pointers must be divided by the size of what they're referencing
+        auto lref = cdk::reference_type::cast(node->left()->type());
+        _pf.INT(std::max(static_cast<size_t>(1), lref->referenced()->size()));
+        _pf.DIV();
+    }
 }
 void til::postfix_writer::do_mul_node(cdk::mul_node* const node, int lvl) {
     ASSERT_SAFE_EXPRESSIONS;
